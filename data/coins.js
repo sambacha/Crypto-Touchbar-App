185fe5bb4610c576825761aafda350c57d710aa7
sam <sam@freighttrust.com>
2021-03-13 16:23:58 -0800

    fix module exports
    Signed-off-by: sam <sam@freighttrust.com>

--- data/coins.js
+++ data/coins.js
@@ -17,75 +17,14 @@ function data() {
 			"Ticker": "SUSHI",
 			"Icon": "../../../img/temp/sushi",
 			"Colour": "F07AB2",
-			"ShowDefault": true
+			"ShowDefault": true,
 		},
 		{
 			"Name": "Yearn Finance",
 			"Ticker": "YFI",
 			"Icon": "../../../img/temp/yfi",
 			"Colour": "2F80ED",
-			"ShowDefault": true
-		},
-		{
-			"Name": "AudioCoin",
-			"Ticker": "ADC",
-			"Icon": "ADC",
-			"Colour": "5DADDE"
-		},
-		{
-			"Name": "AeonCoin",
-			"Ticker": "AEON",
-			"Icon": "AEON-alt",
-			"Colour": "383E47"
-		},
-		{
-			"Name": "Synereo",
-			"Ticker": "AMP",
-			"Icon": "AMP",
-			"Colour": "4FA9DD"
-		},	
-		{
-			"Name": "Anoncoin",
-			"Ticker": "ANC",
-			"Icon": "ANC",
-			"Colour": "A6A6A6"
-		},
-		{
-			"Name": "ArchCoin",
-			"Ticker": "ARCH",
-			"Icon": "ARCH",
-			"Colour": "#071F40"
-		},
-		{
-			"Name": "Ardor",
-			"Ticker": "ARDR",
-			"Icon": "ARDR-alt",
-			"Colour": "4F87C2"
-		},
-		{
-			"Name": "ARK",
-			"Ticker": "ARK",
-			"Icon": "ARK-alt",
-			"Colour": "4B96C0"
-		},
-		{
-			"Name": "Aurora Coin",
-			"Ticker": "AUR",
-			"Icon": "AUR-alt",
-			"Colour": "80B17C"
-		},
-		// B - 15, 2 custom
-		{
-			"Name": "BANX",
-			"Ticker": "BANX",
-			"Icon": "BANX",
-			"Colour": "FFFFFF"
-		},
-		{
-			"Name": "BAT",
-			"Ticker": "BAT",
-			"Icon": "BAT-alt",
-			"Colour": "912D61"
+			"ShowDefault": true,
 		},
 		{
 			"Name": "Binance Coin",
@@ -98,94 +37,62 @@ function data() {
 			"Ticker": "BTC",
 			"Icon": "BTC",
 			"Colour": "F38208",
-			"ShowDefault": true
+			"ShowDefault": true,
 		},
-		{
-			"Name": "BitcoinDark",
-			"Ticker": "BTCD",
-			"Icon": "BTCD",
-			"Colour": "ED6E2E"
-		},
-		{
-			"Name": "BitMark",
-			"Ticker": "BTM",
-			"Icon": "BTM-alt",
-			"Colour": "2E0B62"
-		},
-		{
-			"Name": "Bitshares",
-			"Ticker": "BTS",
-			"Icon": "BTS",
-			"Colour": "2D6C92"
-		},
-		// C - 2
-		{
-			"Name": "CLAMS",
-			"Ticker": "CLAM",
-			"Icon": "CLAM-alt",
-			"Colour": "BAC5C0"
-		},
-		{
-			"Name": "CloakCoin",
-			"Ticker": "CLOAK",
-			"Icon": "CLOAK-alt",
-			"Colour": "CD4D32"
-		},
-		// D - 9
 		{
 			"Name": "Compound",
 			"Ticker": "COMP",
 			"Icon": "../../../img/temp/comp",
 			"Colour": "00D395",
-			"ShowDefault": true
+			"ShowDefault": true,
 		},
 		{
 			"Name": "DAO",
 			"Ticker": "DAO",
 			"Icon": "DAO-alt",
-			"Colour": "FFFFFF"
+			"Colour": "FFFFFF",
 		},
 		{
 			"Name": "Polygon",
 			"Ticker": "POLY",
 			"Icon": "../../../img/temp/polygon",
 			"Colour": "FFFFFF",
-			"ShowDefault": true
+			"ShowDefault": true,
 		},
 		{
-			"Name": "CurveFi",
-			"Ticker": "CURV",
+			"Name": "curvefi",
+			"Ticker": "CRV",
 			"Icon": "../../../img/temp/curve",
 			"Colour": "FFFFFF",
-			"ShowDefault": true
+			"ShowDefault": true,
 		},
 		{
 			"Name": "Bulbasaur",
 			"Ticker": "BULB",
 			"Icon": "../../../img/temp/bulb",
 			"Colour": "00D395",
-			"ShowDefault": true
+			"ShowDefault": true,
 		},
 		{
 			"Name": "Manifold Finance",
-			"Ticker": "FOLD",
+			"Ticker": "YAM",
 			"Icon": "../../../img/temp/manifold",
 			"Colour": "0657F9",
-			"ShowDefault": true
+			"ShowDefault": true,
 		},
 		{
 			"Name": "ERC",
 			"Ticker": "ERC",
 			"Icon": "ERC",
 			"Colour": "FFFFFF",
-			"ShowDefault": true
+			"ShowDefault": true,
 		},
 		{
 			"Name": "Ethereum",
 			"Ticker": "ETH",
 			"Icon": "ETH",
 			"Colour": "8B92AF",
-			"ShowDefault": true
+			"ShowDefault": true,
 		},
 		{
 			"Name": "Ethereum Classic",
@@ -195,3 +102,5 @@ function data() {
 		}
 	]
 };
+
+module.exports = _;
